0675ddfa4046999f38f8c09d0f67c764af79a90d
tebri-artanto <tebriartanto@gmail.com>
2024-08-18 22:07:34 +0700

    update loginValidator

--- module/utils/LoginValidator.js
+++ module/utils/LoginValidator.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const LoginValidator = Joi.object({
     username: Joi.string().required(),
     password: Joi.string().min(1).max(255).required(),
+    fcmToken: Joi.string().optional(),
 });
 
 module.exports = LoginValidator;
